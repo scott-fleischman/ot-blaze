2fe76ef4cdd689f98236b7ba1dac977cfbc57c1e
Scott Fleischman <scott.fleischman@logos.com>
2014-04-14 12:57:55 -0700

    Add big endian tests for signed int, including negative.

--- ot-blaze-gtest/ot-blaze-gtest/big_endian.cpp
+++ ot-blaze-gtest/ot-blaze-gtest/big_endian.cpp
@@ -5,5 +5,22 @@ TEST(BigEndian, Uint32)
 {
 	auto bytes = std::array<uint8_t, 4> { 0x04, 0x03, 0x02, 0x01 };
 	auto big_endian = ot::big_endian<uint32_t> { bytes };
-	EXPECT_EQ(0x01020304, big_endian.GetValue());
+	uint32_t expected = 0x04030201;
+	EXPECT_EQ(expected, big_endian.GetValue());
+}
+
+TEST(BigEndian, Int32)
+{
+	auto bytes = std::array<uint8_t, 4> { 0x04, 0x03, 0x02, 0x01 };
+	auto big_endian = ot::big_endian<int32_t> { bytes };
+	int32_t expected = 0x04030201;
+	EXPECT_EQ(expected, big_endian.GetValue());
+}
+
+TEST(BigEndian, NegativeInt32)
+{
+	auto bytes = std::array<uint8_t, 4> { 0xff, 0xff, 0xff, 0xfe };
+	auto big_endian = ot::big_endian<int32_t> { bytes };
+	int32_t expected = -2;
+	EXPECT_EQ(expected, big_endian.GetValue());
 }
